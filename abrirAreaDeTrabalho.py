32b3dd8c5c53c81b72870bb1d9cdb1a4ba240939
caioloureiro <caio.loureiro@hotmail.com>
2026-04-13 08:08:35 -0300

    Reorganizar variáveis e adicionar abertura de WhatsApp e Google Agenda no Firefox

--- abrirAreaDeTrabalho.py
+++ abrirAreaDeTrabalho.py
@@ -3,12 +3,17 @@ import os
 # Opção com 'r' (raw string) - mais fácil
 downloads = r"D:\Downloads"
 sites = r"D:\Sites"
-firefox = r"C:\Program Files\Mozilla Firefox\firefox.exe"
 onedrive = r"C:\Users\Caio\OneDrive"
-filezilla = r"C:\Program Files\FileZilla FTP Client\filezilla.exe"
+filezilla = r"D:\Program Files\FileZilla FTP Client\filezilla.exe"
+
+firefox = r"C:\Program Files\Mozilla Firefox\firefox.exe"
+acessarWhatsAppPeloFirefox = r"C:\Program Files\Mozilla Firefox\firefox.exe https://web.whatsapp.com/"
+acessarAgendaPeloFirefox = r"C:\Program Files\Mozilla Firefox\firefox.exe https://calendar.google.com/calendar/u/0/r"
 
 os.startfile(downloads)
 os.startfile(sites)
 os.startfile(firefox)
 os.startfile(onedrive)
 os.startfile(filezilla)
+os.startfile(acessarWhatsAppPeloFirefox)
+os.startfile(acessarAgendaPeloFirefox)
