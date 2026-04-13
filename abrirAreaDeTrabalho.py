f3f69dd9072edae69a237dcad82c05a1c4110aff
caioloureiro <caio.loureiro@hotmail.com>
2026-04-13 08:00:42 -0300

    Adicionar script para abrir pastas e aplicativos no Windows

---
+++ abrirAreaDeTrabalho.py
@@ -0,0 +1,14 @@
+import os
+
+# Opção com 'r' (raw string) - mais fácil
+downloads = r"D:\Downloads"
+sites = r"D:\Sites"
+firefox = r"C:\Program Files\Mozilla Firefox\firefox.exe"
+onedrive = r"C:\Users\Usuario\OneDrive"
+filezilla = r"C:\Program Files\FileZilla FTP Client\filezilla.exe"
+
+os.startfile(downloads)
+os.startfile(sites)
+os.startfile(firefox)
+os.startfile(onedrive)
+os.startfile(filezilla)
