dc9a3243a86fe430aaecdee11c147eb77a5aa6ec
caioloureiro <caio.loureiro@hotmail.com>
2026-04-20 09:19:13 -0300

    Adicionar script de exemplo com manipulação de strings em Python

---
+++ video31.py
@@ -0,0 +1,14 @@
+frase = "Curso em vídeo Python"
+print(frase)
+print(frase[4])
+print(frase[4:12])
+print(frase[:13])
+print(frase[13:])
+print(frase[::2])
+print(
+"""lorem ipsum dolor sit amet, consectetur adipiscing elit.
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."""
+)
+print(frase.count("o"))
