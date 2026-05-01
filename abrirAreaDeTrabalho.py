1ec72de1927a27d634fd33cf1a3a42d9ff1f5b12
caioloureiro <caio.loureiro@hotmail.com>
2026-05-01 08:36:07 -0300

    Adicionar mensagens de status ao abrir pastas e aplicativos

--- abrirAreaDeTrabalho.py
+++ abrirAreaDeTrabalho.py
@@ -15,12 +15,29 @@ onedrive = r"C:\Users\Caio\OneDrive"
 filezilla = r"D:\Program Files\FileZilla FTP Client\filezilla.exe"
 firefox = r"C:\Program Files\Mozilla Firefox\firefox.exe"
 
+print("Abrindo pastas Downloads...")
 os.startfile(downloads)
+
+print("Abrindo pastas Sites...")
 os.startfile(sites)
+print("Abrindo pastas OneDrive...")
 os.startfile(onedrive)
+print("Abrindo FileZilla...")
 os.startfile(filezilla)
+print("Abrindo Firefox...")
 os.startfile(firefox)
 
+print("Aguardando 2 segundos WhatsApp...")
+time.sleep(2)
+
 webbrowser.open("https://web.whatsapp.com/")
+
+print("Aguardando 2 segundos Google Calendar...")
+time.sleep(2)
+
 webbrowser.open("https://calendar.google.com/calendar/render?tab=wc&pli=1#main_7")
+
+print("Aguardando 2 segundos localhost...")
+time.sleep(2)
+
 webbrowser.open("http://localhost/")
